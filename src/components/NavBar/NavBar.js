d1541a667ba560808d1dd09ba5cda04465d9f356
Fiorella Laveglia <fiolaveglia@gmail.com>
2022-05-10 10:38:35 -0300

    Agregando estilos a la NavBar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,15 +1,16 @@
 import './NavBar.css'
+import logo from './img/logo.png'
 
 const NavBar = () => {
     return (
         <nav>
-            <h1>Ecommerce</h1>
-            <div>
+            <img src= {logo} alt="logo"></img>
+            <div className="navbar">
                 <button>Florales</button>
-                <button>Cítricos</button>
+                <button>Citricos</button>
                 <button>Herbales</button>
-
             </div>
+            <h1>AROMATERAPIA</h1>
         </nav>
     )
 }
