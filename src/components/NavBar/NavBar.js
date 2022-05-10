9efa84c9622a474773030688be7419deac0fe772
Fiorella Laveglia <fiolaveglia@gmail.com>
2022-05-09 22:33:25 -0300

    Clase de jsx y webpack

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,17 @@
+import './NavBar.css'
+
+const NavBar = () => {
+    return (
+        <nav>
+            <h1>Ecommerce</h1>
+            <div>
+                <button>Florales</button>
+                <button>Cítricos</button>
+                <button>Herbales</button>
+
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
