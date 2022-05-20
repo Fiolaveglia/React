4c27b941ca207cef15cd2e4d8a2208abd4af4077
Fiorella Laveglia <fiolaveglia@gmail.com>
2022-05-20 11:44:59 -0300

    Ultima modificacion

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -1,21 +1,21 @@
 import {useState} from 'react'
 import './ItemCount.css';
 
-const ItemCount = ({stock, inicial}) => {
-    const [count, setCount] = useState(inicial)
+const ItemCount = (props) => {
+    const [count, setCount] = useState(props.inicial)
     
     
     const decrement = () => {
         setCount(count - 1)
-        if (count <= inicial) {
-            setCount(inicial)
+        if (count <= props.inicial) {
+            setCount(props.inicial)
         } 
     }
 
     const increment = () => {
         setCount(count + 1)
-        if (count === stock) {
-            setCount(stock)
+        if (count === props.stock) {
+            setCount(props.stock)
         }
     }
 
