9da5d413f714d93690555943119cd1f19ee2f113
Fiorella Laveglia <fiolaveglia@gmail.com>
2022-05-28 19:12:59 -0300

    Modificaciones

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -6,7 +6,7 @@ const ItemDetail = ({detalle}) => {
     return (
         <div>
             <h2>Detalle del producto</h2>
-            <Card key={detalle.id} style={{ width: '18rem' }}>
+            <Card key={detalle.id} style={{ width: '35rem'}}>
                 <Card.Img variant="top" src={detalle.img} alt={detalle.nombre} />
                 <Card.Body>
                     <Card.Title><h3>Aceite de {detalle.nombre}</h3></Card.Title>
